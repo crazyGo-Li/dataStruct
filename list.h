885067c621aa55a0041a5c57b2c299f87ff02f04
crazyGo-Li <15010091457@163.com>
2020-06-01 23:39:12 +0800

    add scan

--- list.h
+++ list.h
@@ -23,6 +23,7 @@ typedef struct List_
 
 void list_init(List *list, void (*destroy)(void *data));
 void list_destroy(List *list);
+void list_scan(List *list);
 
 int list_ins_next(List *list, ListElmt *element, const void *data);
 int list_rem_next(List *list, ListElmt *element, void **data);
