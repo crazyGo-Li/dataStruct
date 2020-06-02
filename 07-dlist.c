bb6c49c31963a9554d837374c24533d4200ad7bb
crazyGo-Li <15010091457@163.com>
2020-06-02 23:39:17 +0800

    add double list

--- 07-dlist.c
+++ 07-dlist.c
@@ -27,4 +27,46 @@ void dlist_destroy(DList *list)
     return;
 }
 
+int dlist_ins_next(DList *list, DListElm *element, const void *data)
+{
+    DListElm *new_element;
 
+    /* can not insert null to list */
+    if(element == NULL && dlist_size(list) != 0)
+    {
+        return -1;
+    }
+
+    /* allocate storage for new element */
+    new_element = (DListElm *)malloc(sizeof(DListElm));
+    if(NULL == new_element)
+    {
+        return -1;
+    }
+
+    /* insert new element to list */
+    new_element->data = (void *)data;
+    if(dlist_size(list) == 0)
+    {
+        list->head = new_element;
+        list->head->prev = NULL;
+        list->head->next = NULL;
+        list->tail = new_element;
+    }
+    else
+    {
+        new_element->next = element->next;
+        new_element->prev = element;
+        if(element->next == NULL)
+        {
+            list->tail = new_element;
+        }
+        else
+        {
+            element->next->prev = new_element;
+        }
+        element->next = new_element;
+    }
+    list->size++;
+    return 0;
+}
