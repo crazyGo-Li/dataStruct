d6770ae3eef9349d9c71f107b9d7d035bd2406d1
crazyGo_Li <15010091457@163.com>
2020-06-11 22:54:42 +0800

    add stack

---
+++ 09-stack.c
@@ -0,0 +1,13 @@
+#include "stack.h"
+#include "list.h"
+#include <stdlib.h>
+
+int stack_push(Stack *stack, const void *data)
+{
+    return list_ins_next(stack, NULL, data);
+}
+
+int stack_pop(Stack *stack, void **data)
+{
+    return list_rem_next(stack, NULL, data);
+}
